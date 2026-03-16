b812da0d16fbedb637d93c52cec5d962cfe422ef
Aditya Dev <adityadev1222004@gmail.com>
2026-03-16 20:15:05 +0530

    Update TechStackData.js

--- src/data/techStackData.js
+++ src/data/techStackData.js
@@ -1,27 +1,27 @@
 
-import html from "../images/SkillsIcon/HTML.png" ; 
-import css from "../images/SkillsIcon/CSS.png" ; 
-import js from "../images/SkillsIcon/javascript.svg" ; 
-import react from "../images/SkillsIcon/React.png" ; 
-import node from "../images/SkillsIcon/NodeJs.svg" ; 
-import express from "../images/SkillsIcon/Express.png" ; 
-import mongodb from "../images/SkillsIcon/MongoDb.svg" ; 
-import tailwind from "../images/SkillsIcon/Tailwind.png" ; 
-import redux from "../images/SkillsIcon/Redux.svg" ; 
-import git from "../images/SkillsIcon/Git.svg" ; 
-import github from  "../images/SkillsIcon/Github.svg" ; 
-import vercel from  "../images/SkillsIcon/Vercel.svg"; 
+import htmlIcon from "../images/SkillsIcon/HTML.png" ; 
+import cssIcon from "../images/SkillsIcon/CSS.png" ; 
+import jsIcon from "../images/SkillsIcon/javascript.svg" ; 
+import reactIcon from "../images/SkillsIcon/React.png" ; 
+import nodeIcon from "../images/SkillsIcon/NodeJs.svg" ; 
+import expressIcon from "../images/SkillsIcon/Express.png" ; 
+import mongodbIcon from "../images/SkillsIcon/MongoDb.svg" ; 
+import tailwindIcon from "../images/SkillsIcon/Tailwind.png" ; 
+import reduxIcon from "../images/SkillsIcon/Redux.svg" ; 
+import gitIcon from "../images/SkillsIcon/Git.svg" ; 
+import githubIcon from  "../images/SkillsIcon/Github.svg" ; 
+import vercelIcon from  "../images/SkillsIcon/Vercel.svg"; 
 export const techStackData = [
-  { name: "HTML", icon: html },
-  { name: "CSS", icon: css },
-  { name: "JavaScript", icon: js },
-  { name: "React", icon: react },
-  { name: "Node.js", icon: node },
-  { name: "Express", icon: express },
-  { name: "MongoDB", icon: mongodb },
-  { name: "Tailwind", icon: tailwind },
-  { name: "Redux", icon: redux },
-  { name: "Git", icon: git },
-  { name: "GitHub", icon: github},
-  { name: "Vercel", icon:  vercel}
+  { name: "HTML", icon: htmlIcon },
+  { name: "CSS", icon: cssIcon },
+  { name: "JavaScript", icon: jsIcon },
+  { name: "React", icon: reactIcon },
+  { name: "Node.js", icon: nodeIcon },
+  { name: "Express", icon: expressIcon },
+  { name: "MongoDB", icon: mongodbIcon },
+  { name: "Tailwind", icon: tailwindIcon },
+  { name: "Redux", icon: reduxIcon },
+  { name: "Git", icon: gitIcon },
+  { name: "GitHub", icon: githubIcon},
+  { name: "Vercel", icon:  vercelIcon}
 ];
